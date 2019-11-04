126f8a7972094a2a4a24df455d5eb92f1abe4d36
mkrause <maikelkrause@gmail.com>
2019-11-04 15:18:31 +0100

    Update ObjectUtil.ts.

--- src/util/ObjectUtil.ts
+++ src/util/ObjectUtil.ts
@@ -1,9 +1,20 @@
 
-export const hasOwnProperty = (obj : {}, propKey : PropertyKey) =>
+type ValuesOf<O extends object> = O[keyof O];
+
+
+// Version of `hasOwnProperty` that doesn't rely on the prototype (to prevent possible overrides)
+export const hasProp = (obj : object, propKey : PropertyKey) =>
     Object.prototype.hasOwnProperty.call(obj, propKey);
 
 
-export const isPlainObject = (obj : {}) => {
+// Check if the given value is an object (non-null)
+export const isObject = (obj : unknown) : obj is object => {
+    return typeof obj === 'object' && obj !== null;
+};
+
+
+// Check if the given object is a plain object (prototype either `null` or `Object.prototype`)
+export const isPlainObject = (obj : unknown) : obj is object => {
     if (typeof obj !== 'object' || obj === null) {
         return false;
     }
@@ -13,11 +24,12 @@ export const isPlainObject = (obj : {}) => {
 };
 
 
-export const mapValues = <O extends {}, Result>(obj : O, fn : (value : O[keyof O], key : keyof O) => Result)
-    : { [key in keyof O] : Result } => {
-        const result : { [key : string] : Result } = {};
+// Map over the values of the given object
+export const mapValues = <O extends object, Result>
+    (obj : O, fn : (value : ValuesOf<O>, key : keyof O) => Result) : { [key in keyof O] : Result } => {
+        const result = {} as { [key in keyof O] : Result };
         for (const key in obj) {
             result[key] = fn(obj[key], key);
         }
-        return result as any;
+        return result;
     };
