dd11b9818b27c269425e3e262c7d02a07dff56e9
mkrause <maikelkrause@gmail.com>
2019-11-24 23:01:58 +0100

    Update concatUri.

--- src/util/uri.ts
+++ src/util/uri.ts
@@ -1,2 +1,17 @@
 
-export const concatUri = (parts : string[]) => parts.filter(part => part !== '').join('/');
+// Take a list of uri path components, and concat them to a single uri path. 
+export const concatUri = (parts : string[]) => parts
+    .filter(part => part !== '')
+    .map((part, index) => {
+        let partTrimmed = part;
+        partTrimmed = partTrimmed.replace(/[/]*$/, ''); // Remove trailing slashes
+        
+        if (index > 0) {
+            partTrimmed = partTrimmed.replace(/^[/]*/, ''); // Remove leading slashes
+        } else {
+            partTrimmed = partTrimmed.replace(/[/]+$/, '/'); // Deduplicate leading slashes
+        }
+        
+        return partTrimmed;
+    })
+    .join('/');
