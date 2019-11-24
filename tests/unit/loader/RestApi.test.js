3cd07d89b817c7d5464052e615e4cd343eeeeb60
mkrause <maikelkrause@gmail.com>
2019-11-25 00:34:44 +0100

    Implement test for RestApi.

--- tests/unit/loader/RestApi.test.js
+++ tests/unit/loader/RestApi.test.js
@@ -6,46 +6,37 @@ import $uri from 'uri-tag';
 import $msg from 'message-tag';
 
 import createAgent from '../../../lib-esm/agent.js';
-import RestApi from '../../../lib-esm/loader/RestApi.js';
+import { Unknown } from '../../../lib-esm/schema/Schema.js';
+
+import { resourceDef } from '../../../lib-esm/loader/Resource.js';
 import ItemResource from '../../../lib-esm/loader/ItemResource.js';
+import CollectionResource from '../../../lib-esm/loader/CollectionResource.js';
+
+import RestApi from '../../../lib-esm/loader/RestApi.js';
 
 
 describe('RestApi', () => {
-    describe('item resource', () => {
-        const agentMock = createAgent({
-            adapter: request => {
-                const { method, url, params } = request;
-                
-                if (url === '/') {
-                    const users = [
-                        { user_id: 'user42', name: 'John' },
-                        { user_id: 'user43', name: 'Alice' },
-                    ];
-                    
-                    let data = users;
-                    
-                    if (typeof params === 'object' && params && params.filters) {
-                        if (params.filters.name) {
-                            data = users.filter(user => user.name === params.filters.name);
-                        }
-                    }
-                    
-                    // Simulate an async request
-                    return new Promise(resolve => {
-                        setTimeout(() => {
-                            resolve({ data });
-                        }, 0);
-                    });
-                } else {
-                    throw new Error($msg`Unknown route ${url}`);
-                }
-            },
-        });
+    const agent = createAgent({
+        adapter: async request => { throw new Error(`Not supported`); },
+    });
+    
+    it('should require an agent', () => {
+        expect(() => { RestApi(); }).to.throw(TypeError);
+    });
+    
+    it('should require a resource creator', () => {
+        expect(() => { RestApi(agent); }).to.throw(TypeError);
+    });
+    
+    it('should require a resource creator', () => {
+        expect(() => { RestApi(agent); }).to.throw(TypeError);
+    });
+    
+    it('should return a resource by applying a root context', () => {
+        const api = RestApi(agent, ItemResource(Unknown));
         
-        const api = RestApi(agentMock, ItemResource({}));
-        
-        it('should return a resource of the specified resource type', () => {
-            //...
-        });
+        expect(api).property(resourceDef).property('path').to.deep.equal([]);
+        expect(api).property(resourceDef).property('store').to.deep.equal([]);
+        expect(api).property(resourceDef).property('uri').to.deep.equal('');
     });
 });
